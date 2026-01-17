8f5c4366b7e754dafb52f2ec86e2dc865f1b4096
raghadsb171-prog <raghadsb171@gmail.com>
2026-01-17 12:11:13 +0200

    Delete MedicalSystem directory

--- MedicalSystem/BusinessLogic_Layer/AppointmentController.php
+++
@@ -1,7 +0,0 @@
-<?php
-class AppointmentController {
-    public function requestAppointment($pId, $docId, $date) {
-        // التحقق من المنطق (Business Rules) ثم الحفظ
-        return DAFacade::getInstance()->insertAppointment($pId, $docId, $date);
-    }
-}
--- MedicalSystem/UI_Layer/PatientUI.php
+++
@@ -1,9 +0,0 @@
-<?php
-class PatientUI {
-    public function submitPatientInfo($id, $name, $history) {
-        $success = BLFacade::getInstance()->managePatient($id, $name, $history);
-        if($success) {
-            echo "Patient Record Updated Successfully.";
-        }
-    }
-}
